7986f9e0c38be5ffe9ed698782ef88f0458aef44
jSohnData <jihos@me.com>
2023-05-07 14:00:53 -0400

    UPDATED mozart's ghost link

--- functions/routes/pi.js
+++
@@ -1,9 +0,0 @@
-const urlArray = [
-  "https://assenfuego.com/",
-  "https://jsohndata.github.io/bluenight-in-seoul/",
-  "https://jsohndata.github.io/kardashev-scale/",
-  "https://jsohndata.github.io/bubble-trails/",
-  "https://system-overload-solution.web.app/",
-  "https://jsohndata.github.io/magic-color-xxl/",
-  "https://jsohndata.github.io/cogito-ergo-sum/"
-];
